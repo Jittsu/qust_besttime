17440ff3808ed7f501ddcc35bc7d7f8fe8cf2509
Jittsu <naoto.j3781@gmail.com>
2019-11-06 23:14:55 +0900

    fix

--- best_time/wsgi.py
+++ best_time/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for besttime project.
+WSGI config for best_time project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'besttime.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'best_time.settings')
 
 application = get_wsgi_application()
