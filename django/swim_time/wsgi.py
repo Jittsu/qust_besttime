20122985f854c1b85f61ff8c029351fad1b93dfa
Jittsu <naoto.j3781@gmail.com>
2020-02-01 16:12:52 +0900

    django docker

--- django/swim_time/wsgi.py
+++ django/swim_time/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for best_time project.
+WSGI config for my_docker_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'best_time.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'swim_time.settings')
 
 application = get_wsgi_application()
